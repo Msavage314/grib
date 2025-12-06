60652bb01bb93bd30a8eb987b45d8e3125c25606
Michael Savage <savagemichael1707@gmail.com>
2025-12-06 11:19:54 +0000

    Updated game object.

--- grib/core/gameobject.py
+++ grib/core/gameobject.py
@@ -1,5 +1,5 @@
-
 class GameObject:
     """Base class for objects that can live on the grid"""
-    def __init__(self,display_char = "?")
-    
+
+    def __init__(self, display_char="?"):
+        pass
